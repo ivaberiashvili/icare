663e0fe9c71f7c701c8f0c87955825c6881b577a
ivaberiashvili <94993515+ivaberiashvili@users.noreply.github.com>
2025-03-12 16:10:39 +0400

    Day 3: Added Categories & Comments with Relationships
    - Added Category model & linked it to Product
    - Created Comment model linked to Post
    - Registered models in Django Admin
    - Applied migrations & tested relationships in Django shell

--- icare/core/admin.py
+++ icare/core/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Product, Category, Comment
+
+
 # Register your models here.
+class CommentAdmin(admin.ModelAdmin):
+    list_dislay = ('post', 'text', 'created_at')
+    search_field = ('post__title', 'text')
+
+
 
 admin.site.register(Post)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Comment, CommentAdmin)
--- icare/core/models.py
+++ icare/core/models.py
@@ -8,3 +8,36 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment on {self.post.title}"
+
+class Category (models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']  # Orders categories alphabetically
+
+    def __str__(self):
+        return self.name
+
+class Product (models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+
+
+
+    
