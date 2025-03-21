5c25ea12edbfea968eb9d1d4ec53e011814d58c9
ivaberiashvili <94993515+ivaberiashvili@users.noreply.github.com>
2025-03-22 00:05:13 +0400

    Added drf-spectacular for API documentation

--- icare/icare/settings.py
+++ icare/icare/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- icare/icare/urls.py
+++ icare/icare/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
 from django.urls import path, include
 from core.views import HomeView, APIView, PostListView
-from core.views import RankedProductsView
-from core.views import ProductListCreate, ProductRetrieveUpdateDestroy
-
+from core.views import RankedProductsView, ProductListCreate, ProductRetrieveUpdateDestroy
+from rest_framework import permissions
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +30,13 @@ urlpatterns = [
     path('blog/', PostListView.as_view()),
     path('ranked-products/', RankedProductsView.as_view(), name='ranked-products'),
     path('api-auth/', include('rest_framework.urls')),
+
+    # DRF Product API
     path('products/', ProductListCreate.as_view(), name='product-list'),
     path('products/<int:pk>/', ProductRetrieveUpdateDestroy.as_view(), name='product-detail'),
+
+    # drf-spectacular schema and docs
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
