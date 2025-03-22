cf341343a31e225cbe93f5f7d3aeb289f69b1452
ivaberiashvili <94993515+ivaberiashvili@users.noreply.github.com>
2025-03-22 14:05:35 +0400

    Exercise 1 : added basic auth

--- icare/core/views.py
+++ icare/core/views.py
@@ -11,6 +11,9 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
+from rest_framework.permissions import IsAuthenticated
+
+
 
 # Create your views here.
 class HomeView(View):
@@ -41,7 +44,9 @@ class RankedProductsView(View):
 class ProductListCreate(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
 
 class ProductRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
--- icare/icare/settings.py
+++ icare/icare/settings.py
@@ -121,4 +121,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
