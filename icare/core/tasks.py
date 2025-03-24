0cf781044509f304783983ad7cf3abad7bbb0655
ivaberiashvili <94993515+ivaberiashvili@users.noreply.github.com>
2025-03-24 22:30:50 +0400

    Day 8 / Exercise 8: add Celery Beat support and scheduled daily_summary task

--- icare/core/tasks.py
+++ icare/core/tasks.py
@@ -3,3 +3,7 @@ from celery import shared_task
 @shared_task
 def send_welcome_email(user_id):
     print(f"📨 Sent welcome email to user {user_id}")
+
+@shared_task
+def daily_summary():
+    print("📊 Daily summary task ran.")
