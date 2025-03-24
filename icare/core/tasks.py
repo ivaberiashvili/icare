72126cddda18a548c3a40b1868480e4435ae408e
ivaberiashvili <94993515+ivaberiashvili@users.noreply.github.com>
2025-03-24 22:39:24 +0400

    Day 8 Complete /  Exercise 4: Add retry logic to Celery task with self.retry

--- icare/core/tasks.py
+++ icare/core/tasks.py
@@ -1,4 +1,6 @@
 from celery import shared_task
+import random
+
 
 @shared_task
 def send_welcome_email(user_id):
@@ -7,3 +9,10 @@ def send_welcome_email(user_id):
 @shared_task
 def daily_summary():
     print("📊 Daily summary task ran.")
+
+@shared_task(bind=True, max_retries=3)
+def risky_task(self):
+    print("⚠️ Running risky task...")
+    if random.random() < 0.5:
+        raise self.retry(exc=Exception("Random failure!"), countdown=5)
+    print("✅ Risky task succeeded!")
